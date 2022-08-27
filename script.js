6baea96d8c419e30dc4104ec8df10e5484c37cba
Arman Shahariar <111569490+armanshahriar@users.noreply.github.com>
2022-08-27 10:39:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+ var base =// parseInt(
+    prompt("Enter base :")//)
+var height =// parseInt(
+   prompt("Enter height :")//)
+
+area = base * height 
+
+document.write("Area = "+area+"<br>")
+
+
+
+var base = parseFloat(prompt("Enter base :"))
+var height = parseFloat(prompt("Enter height :"))
+
+area = base * height 
+
+document.write("Area = "+area+"<br>")
+
+var base = parseInt(prompt("Enter base :"))
+var height = parseInt(prompt("Enter height :"))
+
+area = base * height 
+
+document.write("Area = "+area+"<br>")
+
+var base = parseInt(prompt("Enter base :"))
+var height = parseInt(prompt("Enter height :"))
+
+area =(base * height)/2
+
+document.write("Area = "+area+"<br>")
